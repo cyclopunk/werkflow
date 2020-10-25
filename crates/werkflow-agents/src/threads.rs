750eb00acfbaf188c36d5f3ddfa6dd4417191aa8
Adam Shaw <adam.shaw@slalom.com>
2020-10-25 12:11:55 -0400

    Forgot this

--- crates/werkflow-agents/src/threads.rs
+++ crates/werkflow-agents/src/threads.rs
@@ -4,10 +4,10 @@ use anyhow::anyhow;
 pub(crate) struct AsyncRunner;
 
 impl AsyncRunner {
-    pub(crate) fn block_on<'a, F: Future + Sync + Send + 'static>(f: F) -> F::Output where F::Output : Sync + Send + 'static { 
-        let task = tokio::task::spawn_blocking(move || futures::executor::block_on( f ) );
+    pub(crate) fn block_on<'a, F: Future +  Send + 'static>(f: F) -> F::Output where F::Output : Sync + Send + 'static { 
+        let task = tokio::task::spawn_blocking(move || async_std::task::block_on( f ) );
 
-        futures::executor::block_on( task ) 
-            .map_err(|err| anyhow!("Couldn't run future")).unwrap()
+        async_std::task::block_on( task ) 
+            .map_err(|_err| anyhow!("Couldn't run future")).unwrap()
     }
 }
